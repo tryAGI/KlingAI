55820f101dc7dfb8bfa3a9cdda907e97621eb259
Konstantin S. <havendv@gmail.com>
2026-04-01 05:32:46 +0400

    feat: Updated OpenAPI spec (#9)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/KlingAI/Generated/KlingAI.KlingAIClient.CreateVirtualTryOn.g.cs
+++ src/libs/KlingAI/Generated/KlingAI.KlingAIClient.CreateVirtualTryOn.g.cs
@@ -244,6 +244,7 @@ namespace KlingAI
             string? callbackUrl = default,
             global::System.Threading.CancellationToken cancellationToken = default)
         {
+
             var __request = new global::KlingAI.CreateVirtualTryOnRequest
             {
                 ModelName = modelName,
